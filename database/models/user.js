f81de662da27d9224667a775b8d8a6b3456204df
Derrick Mei <derrick.mei.168@gmail.com>
2020-07-24 02:38:07 -0500

    changed setter for user password to be auto hashed

--- database/models/user.js
+++ database/models/user.js
@@ -1,7 +1,18 @@
-module.exports = (sequelize, DataTypes) => {
+const bcrypt = require("bcryptjs");
+const secrets = require('../../secrets');
+// const hash = async (password) => await bcrypt.hash(password, 12);
+
+module.exports = async (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     username: DataTypes.STRING,
-    password: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      set(password) {
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+        this.setDataValue("password", hashedPassword);
+      },
+    },
   });
 
   User.associate = (models) => {
