1ec86e3208061b88dc69a8b38404e8363f4543bf
Derrick Mei <derrick.mei.168@gmail.com>
2020-08-01 02:43:31 -0500

    extracted middleware

---
+++ http/middleware.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const secrets = require("../secrets");
+
+function restricted(req, res, next) {
+  try {
+    const token = req.cookies.token;
+    const decoded = jwt.verify(token, secrets.secret);
+    if (decoded) {
+      req.user = decoded;
+      next();
+    }
+  } catch (err) {
+    next({ status: 401, message: "Invalid Credentials", err });
+  }
+}
+
+function sanityCheck(req, res, next) {
+  res.json({ message: "I work" });
+}
+
+// Wrong Route Handler
+function wrongRoute(req, res, next) {
+  res.status(401).json({ error: "Route does not exist" });
+}
+
+// Global Error Handler
+const globalErrorHandler = (err, req, res, next) => {
+  console.log("Global Error: ", err);
+  res.status(err.status || 500).json({ err });
+};
+
+module.exports = { restricted, sanityCheck, wrongRoute, globalErrorHandler };
