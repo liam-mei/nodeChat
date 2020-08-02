a89f7ef29318fc7a08dbb37b61316060633bf566
Derrick Mei <derrick.mei.168@gmail.com>
2020-08-02 02:45:13 -0500

    created protected routes listrooms listrooms/query

---
+++ http/protectedRouter.js
@@ -0,0 +1,30 @@
+const express = require("express");
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
+// const secrets = require("../secrets");
+// const db = require("../database/models");
+const { RoomAccessModel, RoomUserAccessModel } = require("../accessModels");
+
+const router = express.Router({
+  mergeParams: true,
+});
+
+router.get("/listrooms", async (req, res, next) => {
+  try {
+    res.json(await RoomAccessModel.find());
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/listrooms/:query", async (req, res, next) => {
+  const { query } = req.params;
+  try {
+    res.json(await RoomAccessModel.find({ name: query }));
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+module.exports = router;
