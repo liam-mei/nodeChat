c281f0b663559ff62b0ec90491242322ca5aa444
Derrick Mei <derrick.mei.168@gmail.com>
2020-08-05 01:02:23 -0500

    updated message.js to use snake case

--- database/models/message.js
+++ database/models/message.js
@@ -1,17 +1,23 @@
 module.exports = (sequelize, DataTypes) => {
-  const Message = sequelize.define("Message", {
-    message: {
-      type: DataTypes.STRING,
+  const Message = sequelize.define(
+    "Message",
+    {
+      message: {
+        type: DataTypes.STRING,
+      },
+      createdAt: { type: DataTypes.DATE, field: "created_at" },
+      updatedAt: { type: DataTypes.DATE, field: "updated_at" },
     },
-  });
+    { tableName: "messages" }
+  );
 
   Message.associate = (models) => {
     Message.belongsTo(models.User, {
-      // foreignKey: "user_id",
+      foreignKey: "user_id",
       // onDelete: "SET NULL",
     });
     Message.belongsTo(models.Room, {
-      // foreignKey: "room_id",
+      foreignKey: "room_id",
       // onDelete: "SET NULL",
     });
   };
