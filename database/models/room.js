6ac7390a8a2d81030a10966d578cb765e2d6c354
Derrick Mei <derrick.mei.168@gmail.com>
2020-08-05 01:02:48 -0500

    updated room.js to use snake case

--- database/models/room.js
+++ database/models/room.js
@@ -1,11 +1,17 @@
 module.exports = (sequelize, DataTypes) => {
-  const Room = sequelize.define("Room", {
-    name: DataTypes.STRING,
-  });
+  const Room = sequelize.define(
+    "Room",
+    {
+      name: DataTypes.STRING,
+      createdAt: { type: DataTypes.DATE, field: "created_at" },
+      updatedAt: { type: DataTypes.DATE, field: "updated_at" },
+    },
+    { tableName: "rooms" }
+  );
 
   Room.associate = (models) => {
     Room.hasMany(models.Message, {
-      // foreignKey: "message_id",
+      foreignKey: "room_id"
       // onDelete: "SET NULL",
     });
     Room.belongsToMany(models.User, {
