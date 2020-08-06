d410d5c022335d80035519b2b204d8c59ff7410c
Derrick Mei <derrick.mei.168@gmail.com>
2020-08-06 01:03:29 -0500

    updated roomusers.js to use snake case

--- database/models/roomUser.js
+++ database/models/roomUser.js
@@ -1,8 +1,14 @@
 module.exports = (sequelize, DataTypes) => {
-  const RoomUser = sequelize.define("RoomUser", {
-    userId: DataTypes.INTEGER,
-    roomId: DataTypes.INTEGER,
-  });
+  const RoomUser = sequelize.define(
+    "RoomUser",
+    {
+      user_id: DataTypes.INTEGER,
+      room_id: DataTypes.INTEGER,
+      createdAt: { type: DataTypes.DATE, field: "created_at" },
+      updatedAt: { type: DataTypes.DATE, field: "updated_at" },
+    },
+    { tableName: "room_users" }
+  );
 
   RoomUser.associate = (models) => {};
 
