faa5d04fb4ec8ee82b2471c2bd5da8f2b1bfaa8d
Derrick Mei <derrick.mei.168@gmail.com>
2020-08-06 01:03:40 -0500

    updated users.js to use snake case

--- database/models/user.js
+++ database/models/user.js
@@ -1,21 +1,27 @@
 const bcrypt = require("bcryptjs");
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
-    username: DataTypes.STRING,
-    password: {
-      type: DataTypes.STRING,
-      set(password) {
-        const salt = bcrypt.genSaltSync(10);
-        const hashedPassword = bcrypt.hashSync(password, salt);
-        this.setDataValue("password", hashedPassword);
+  const User = sequelize.define(
+    "User",
+    {
+      username: DataTypes.STRING,
+      password: {
+        type: DataTypes.STRING,
+        set(password) {
+          const salt = bcrypt.genSaltSync(10);
+          const hashedPassword = bcrypt.hashSync(password, salt);
+          this.setDataValue("password", hashedPassword);
+        },
       },
+      createdAt: { type: DataTypes.DATE, field: "created_at" },
+      updatedAt: { type: DataTypes.DATE, field: "updated_at" },
     },
-  });
+    { tableName: "users" }
+  );
 
   User.associate = (models) => {
     User.hasMany(models.Message, {
-      // foreignKey: "message_id",
+      foreignKey: 'user_id'
       // onDelete: "SET NULL",
     });
     User.belongsToMany(models.Room, {
