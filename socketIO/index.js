d2351c0c068056719543ec551ed0a6d0304a1457
Derrick Mei <derrick.mei.168@gmail.com>
2020-08-14 01:27:33 -0500

    updated sockets to add more functions to join/leave room and send message

--- socketIO/index.js
+++ socketIO/index.js
@@ -1,13 +1,36 @@
 const socketIO = require("socket.io");
 
 const httpServer = require("../http/httpServer");
-const db = require("../database/models");
+const {
+  MessageAccessObject,
+  RoomAccessObject,
+} = require("../dataAccessObjects");
 
 const io = socketIO(httpServer);
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log("a client connected");
-  socket.emit('rooms', [1,2,3])
+
+  socket.on("getRooms", async () => {
+    console.log("getting Rooms");
+    const currentRooms = await RoomAccessObject.find();
+
+    socket.emit("rooms", currentRooms);
+  });
+
+  socket.on("joinRoom", async (id) => {
+    const roomToLeave = Object.keys(socket.rooms)[1];
+    socket.leave(roomToLeave);
+    socket.join(id);
+    console.log(`someone joined roomId: ${id}`);
+
+    const roomMessages = await MessageAccessObject.find({ room_id: id });
+    socket.emit("currentMessages", roomMessages);
+  });
+
+  socket.on("sendMessage", (data) => {
+    socket.to(data.room).emit("newMessage", data.message);
+  });
 });
 
 module.exports = io;
