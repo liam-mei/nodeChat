d0c82691ca0e0a47b18fcf124b5ebdc23d510583
Derrick Mei <derrick.mei.168@gmail.com>
2020-09-14 04:36:10 -0500

    updated login route returns token and checks for username and password

--- http/userRouter.js
+++ http/userRouter.js
@@ -12,17 +12,16 @@ const router = express.Router({
 router.post("/login", async (req, res, next) => {
   const { username, password } = req.body;
 
-  if (!username || !password)
+  if (!username || !password) {
     next({
       status: 400,
       message: "please fill out both username and password",
     });
+    return;
+  }
 
   try {
     const user = await UserAccessObject.findOne({ username });
-    if (!user) {
-      next({ status: 400, message: "username not valid" });
-    }
 
     const passwordValid = await bcrypt.compare(password, user.password);
     if (passwordValid) {
@@ -34,14 +33,8 @@ router.post("/login", async (req, res, next) => {
           expiresIn: "10000000",
         }
       );
-        console.log(`token: ${token}`)
-      // I would use a httpOnly cookie but a token is required to
-      // authenticate for socket
-      // res.cookie("token", token, {
-      //   maxAge: 1000 * 60 * 60 * 24 * 7,
-      //   // httpOnly: false,
-      // })
-      res.json({ message: `Welcome ${user.username}`, token });
+
+      res.json({ message: `Welcome ${user.username}`, token, user });
     } else {
       next({ status: 401, message: "Invalid Credentials" });
     }
@@ -56,16 +49,27 @@ router.post("/register", async (req, res, next) => {
   if (!username || !password)
     next({
       status: 400,
-      message: "please fill out both username and password",
+      error: "please fill out both username and password",
     });
 
   try {
     const userExists = await UserAccessObject.findOne({ username });
-    if (userExists) res.status(400).json({ error: "username already exists" });
+    if (userExists) {
+      next({ status: 400, error: "Username already exists" });
+    } else {
+      const newUser = await UserAccessObject.create({ username, password });
 
-    const newUser = await UserAccessObject.create({ username, password });
+      const token = jwt.sign(
+        { username: user.username, id: user.id },
+        secrets.secret,
+        {
+          // expiresIn: "7d",
+          expiresIn: "10000000",
+        }
+      );
 
-    res.status(201).json(newUser);
+      res.status(201).json({ newUser, token });
+    }
   } catch (error) {
     next(error);
   }
